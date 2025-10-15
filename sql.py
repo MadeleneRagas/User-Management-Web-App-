83d461875edd2bd2cb1ce0940167d885fea63088
unknown <muragas@mymail.mapua.edu.ph>
2025-10-15 21:49:00 +0800

    Project added

---
+++ sql.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import sqlite3
+import pandas as pd
+
+
+def create_table():
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+    c.execute('CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, age INTEGER)')
+    conn.commit()
+    conn.close()
+
+def add_user(name, email, age):
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+    c.execute('INSERT INTO users(name, email, age) VALUES (?, ?, ?)', (name, email, age))
+    conn.commit()
+    conn.close()
+
+def view_users():
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM users')
+    data = c.fetchall()
+    conn.close()
+    return data
+
+def delete_user(user_id):
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+    c.execute('DELETE FROM users WHERE id=?', (user_id,))
+    conn.commit()
+    conn.close()
+    
+st.set_page_config (page_title="User Database App", page_icon="")
+st.title("User Management Web App")
+menu = ["Home", "Add User", "View Users", "Delete User"]
+choice = st.sidebar.selectbox("Menu", menu)
+    
+create_table()  
+
+if choice == "Add User":
+    st.subheader("Add New User")
+    name = st.text_input("Name")
+    email = st.text_input("Email")
+    age = st.number_input("Age", 0, 120)
+    if st.button("Submit"):
+        add_user(name, email, age)
+        st.success(f"{name} added successfully!")
+        
+elif choice == "View Users":
+    st.subheader("View All Users")
+    users = view_users()
+    df = pd.DataFrame(users, columns=["ID", "Name", "Email", "Age"])
+    st.dataframe(df)
+
+elif choice == "Delete User":
+    st.subheader("Delete a User")
+    users = view_users()
+    df = pd.DataFrame(users, columns=["ID", "Name", "Email", "Age"])
+    st.dataframe(df)
+    user_id = st.number_input("Enter ID to delete", min_value=1, step=1)
+    if st.button("Delete"):
+        delete_user(int(user_id))
+        st.warning(f"User {int(user_id)} deleted!")
+
